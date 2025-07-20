b2fc5e577c2f237d45778ec418187d419065f07c
SoongGuanLeong <108955118+SoongGuanLeong@users.noreply.github.com>
2025-07-21 00:56:57 +0800

    Update 07_change_over_time_analysis.sql

--- scripts/07_change_over_time_analysis.sql
+++ scripts/07_change_over_time_analysis.sql
@@ -46,20 +46,3 @@ FROM gold.fact_sales
 WHERE order_date IS NOT NULL
 GROUP BY FORMAT(order_date, 'yyyy-MMM')
 ORDER BY FORMAT(order_date, 'yyyy-MMM');
-
--- Calculate the total sales per month
--- and the running total of sales over time
-SELECT
-order_date,
-total_sales,
-SUM(total_sales) OVER (PARTITION BY YEAR(order_date) ORDER BY order_date) AS running_total_sales,
-AVG(avg_price) OVER (PARTITION BY YEAR(order_date) ORDER BY order_date) AS moving_avg_price
-FROM (
-SELECT
-DATETRUNC(MONTH, order_date) AS order_date,
-SUM(sales_amount) AS total_sales,
-AVG(price) AS avg_price
-FROM gold.fact_sales
-WHERE order_date IS NOT NULL
-GROUP BY DATETRUNC(MONTH, order_date)
-)t
